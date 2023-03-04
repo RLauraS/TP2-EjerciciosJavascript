821886548d96e4575b9e78065d2a08fab8af3a30
RLauraS <R_LauraS@yahoo.com.ar>
2023-03-04 00:40:55 -0300

    Se realiza ejercicio 1

--- js/ejercicio1.js
+++ js/ejercicio1.js
@@ -1,1 +1,12 @@
 /* 1- Escribir un programa que solicite la edad y si es mayor de 18 años mostrar un mensaje que ya puede conducir, si la edad ingresada no es un número válido indicarlo en un mensaje.*/
+const edad = parseInt(prompt("Por favor, ingrese su edad"));
+
+if (edad > 0 || edad < 120) {
+  if (edad > 17) {
+    document.write("Ya puede conducir");
+  } else {
+    document.write("Aún no tiene la edad necesaria para conducir");
+  }
+} else {
+  document.write("Disculpe, la edad ingresada no es válida");
+}
