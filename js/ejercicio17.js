fcc5db3058acf06ccf64d0248ffad7e8978698d4
RLauraS <R_LauraS@yahoo.com.ar>
2023-03-08 17:27:22 -0300

    Se corrige la mala practica de  código comentado

--- js/ejercicio17.js
+++ js/ejercicio17.js
@@ -18,22 +18,4 @@ for (let i = 0; i < texto.length; i++) {
    } 
 }
 document.write("La primera vocal está en la posición " + (posicion+1));
-/*
-var vocales = ["a", "e", "i", "o", "u"];
-var texto = prompt("Introduce un texto");
-var textomin = texto.toLowerCase();
-var posicion = 0;
-var parar = false;
-for (var i = 0; i < textomin.length; i++) {
-    for (var j = 0; j < vocales.length; j++) {
-        if (vocales[j] == textomin.charAt(i)) {
-            posicion = i;
-            parar = true;
-            break;
-        }
-    }
-    if (parar) {
-        break;
-    }
-}
-document.write("La primera vocal está en la posición " + posicion);*/
+
