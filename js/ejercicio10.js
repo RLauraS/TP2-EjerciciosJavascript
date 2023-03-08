fe0cf2dd9c8ac12f7dfaaed0c3997074f7e83dd7
RLauraS <R_LauraS@yahoo.com.ar>
2023-03-08 19:58:18 -0300

    Se realiza ejercicio 10

--- js/ejercicio10.js
+++ js/ejercicio10.js
@@ -3,3 +3,20 @@
 //for por fila
 //for por columna
 //veces = fila x columna
+let filas = parseInt(prompt("Ingrese un número de filas que desee"));
+let columnas = parseInt(prompt("Ingrese un número de columnas que desee"));
+repeticiones = filas * columnas;
+document.write("<table>");
+if (!isNaN(filas) || !isNaN(columnas)) {
+  for (let i = 0; i < filas; i++) {
+    document.write("<tr>");
+    for (let j = 0; j < columnas; j++) {
+      document.write("<td>" + repeticiones + "</td>");
+      repeticiones--;
+    }
+    document.write("</tr>");
+  }
+  document.write("</table>");
+} else {
+  alert("Por favor ingrese números, no palabras");
+}
