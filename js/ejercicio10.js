d0b6345f2d31d627dd64434328b2585713f06802
RLauraS <R_LauraS@yahoo.com.ar>
2023-03-08 20:42:18 -0300

    Se corrigen detalles de ejercicio 10

--- js/ejercicio10.js
+++ js/ejercicio10.js
@@ -1,8 +1,5 @@
 /*10- Realiza un script que pida número de filas y columnas y escriba una tabla. Dentro de cada una de las celdas deberá escribirse un número consecutivo en orden descendente. Si, por ejemplo, la tabla es de 7×5 los números irán del 35 al 1.
  */
-//for por fila
-//for por columna
-//veces = fila x columna
 let filas = parseInt(prompt("Ingrese un número de filas que desee"));
 let columnas = parseInt(prompt("Ingrese un número de columnas que desee"));
 repeticiones = filas * columnas;
